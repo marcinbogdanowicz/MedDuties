f3c40cfe597a896e7f589d707e38fd7b0bdc52e0
marcinbogdanowicz <marcin.bogdanowicz@wp.eu>
2025-01-27 12:29:07 +0100

    Adjust fetch_algoritm.sh script - remove algorithm git repo data after fetching

--- fetch_algorithm.sh
+++ fetch_algorithm.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+echo "Recreating algorithm directory..."
 rm -rf algorithm
 mkdir algorithm
+
 git clone git@github.com:marcinbogdanowicz/MedDutiesRevisited.git algorithm
+
+echo "Removing algorithm .git directory..."
+rm -rf algorithm/.git
+
+echo "Done."
