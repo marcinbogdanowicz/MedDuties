5473ce1be726873c2e6e94b27728953dab1430d8
marcinbogdanowicz <marcin.bogdanowicz@wp.eu>
2025-01-27 19:24:18 +0100

    Add error handling to algorithm api client

--- backend/apps/algorithm/client.py
+++ backend/apps/algorithm/client.py
@@ -1,5 +1,9 @@
+import logging
+
 import requests
 from django.conf import settings
+
+logger = logging.getLogger('django')
 
 
 class AlgorithmAPIClient:
@@ -10,8 +14,12 @@ class AlgorithmAPIClient:
     def make_request(self, path: str, method: str, data: dict | None = None) -> dict:
         url = f"{self.base_url}/{path}"
         response = requests.request(method, url, headers=self.headers, json=data)
-        response.raise_for_status()
-        return response.json()
+        try:
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            logger.exception(e)
+            return {"error": "An error occurred while connecting to the algorithm service."}
 
     def set_duties(self, data: dict) -> dict:
         return self.make_request("set_duties", "POST", data)
