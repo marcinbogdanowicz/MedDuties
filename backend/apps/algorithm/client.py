385a399aee91214883b59bda1fb5a6845d2dcfbc
marcinbogdanowicz <marcin.bogdanowicz@wp.eu>
2025-01-28 21:47:21 +0100

    Refactor error handling on AlgorithmAPIClient

--- backend/apps/algorithm/client.py
+++ backend/apps/algorithm/client.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
 import logging
+from typing import TYPE_CHECKING
 
 import requests
 from django.conf import settings
+from urllib3.exceptions import ConnectionError
+
+if TYPE_CHECKING:
+    from requests import Response
 
 logger = logging.getLogger('django')
 
@@ -13,13 +20,26 @@ class AlgorithmAPIClient:
 
     def make_request(self, path: str, method: str, data: dict | None = None) -> dict:
         url = f"{self.base_url}/{path}"
-        response = requests.request(method, url, headers=self.headers, json=data)
         try:
-            response.raise_for_status()
-            return response.json()
-        except Exception as e:
+            response = requests.request(method, url, headers=self.headers, json=data)
+        except ConnectionError as e:
             logger.exception(e)
-            return {"error": "An error occurred while connecting to the algorithm service."}
+            return {"error": "Algorithm service is unavailable."}
+
+        return self.handle_response(response)
+
+    def handle_response(self, response: Response) -> dict:
+        if response.status_code >= 400:
+            try:
+                return response.json()
+            except requests.exceptions.JSONDecodeError:
+                logger.error(
+                    f'Request to {response.request.url} failed with status code {response.status_code}. '
+                    f'Response: {response.text}'
+                )
+                return {"error": "Request to algorithm service failed."}
+
+        return response.json()
 
     def set_duties(self, data: dict) -> dict:
         return self.make_request("set_duties", "POST", data)
